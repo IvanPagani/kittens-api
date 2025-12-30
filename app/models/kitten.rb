9f17f3d52d67a950fa30a137573fc5f5ca5d8732
IvanPagani <ivanpagani07@gmail.com>
2025-12-30 12:31:18 -0300

    finish simple CRUD

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 30 }
+  validates :cuteness, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :softness, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
